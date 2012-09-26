371a9f1c0403d54e7391eb39bd574e5f98cdb8d7
Muhammad Khan <mhk98@cornell.edu>
2012-09-26 17:54:40 -0400

    think i got the majority of triangulation

--- CS 4620 PA1/src/cs4620/pa1/shape/Cylinder.java
+++ CS 4620 PA1/src/cs4620/pa1/shape/Cylinder.java
@@ -20,15 +20,25 @@ public class Cylinder extends TriangleMesh
 		//we include both faces and the two axis points
 		int vCount = (radial_sectors * 2) + 2;
 		float[] vertices = new float[vCount * 3];
+		ArrayList<Float> topVs = new ArrayList<Float>();
+		ArrayList<Float> topNs = new ArrayList<Float>();
+		ArrayList<Float> bottomVs = new ArrayList<Float>();
+		ArrayList<Float> bottomNs = new ArrayList<Float>();
 		float[] normals = new float[vCount * 3];
 		
 		//putting in the vertices and their corresponding normals
 		vertices[0] = 0;
+		topVs.add(0.0f);
 		vertices[1] = 1;
+		topVs.add(1.0f);
 		vertices[2] = 0;
+		topVs.add(2.0f);
 		normals[0] = 0;
+		topNs.add(0.0f);
 		normals[1] = 1;
+		topNs.add(1.0f);
 		normals[2] = 0;
+		topNs.add(0.0f);
  		
 		int index = 3;
 		for(float theta = 0; theta < 2*Math.PI; theta += radial_interval){
@@ -36,18 +46,30 @@ public class Cylinder extends TriangleMesh
 			Point3f p2 = cylToCar(1.0f, theta + 0.5f*radial_interval, -1.0f);
 			
 			vertices[index] = p.x;
+			topVs.add(p.x);
 			vertices[index + 1] = p.y;
+			topVs.add(p.y);
 			vertices[index + 2] = p.z;
-			normals[index] = (float)(p.x / Math.sqrt((float)(p.x*p.x + 1 + p.z*p.z)));
-			normals[index + 1] = (float)(1.0f / Math.sqrt((float)(p.x*p.x + 1 + p.z*p.z)));
-			normals[index + 2] = (float)(p.z / Math.sqrt((float)(p.x*p.x + 1 + p.z*p.z)));
+			topVs.add(p.z);
+			normals[index] = p.x / (float)Math.sqrt((p.x*p.x + 1 + p.z*p.z));
+			topNs.add(normals[index]);
+			normals[index + 1] = 1.0f /(float)Math.sqrt((p.x*p.x + 1 + p.z*p.z));
+			topNs.add(normals[index + 1]);
+			normals[index + 2] = p.z /(float)Math.sqrt((p.x*p.x + 1 + p.z*p.z));
+			topNs.add(normals[index + 2]);
 			
 			vertices[index + 3] = p2.x;
+			bottomVs.add(p2.x);
 			vertices[index + 4] = p2.y;
+			bottomVs.add(p2.y);
 			vertices[index + 5] = p2.z; // we need to set the normals here as well.
-			normals[index + 3] = (float)(p2.x / Math.sqrt((float)(p2.x*p2.x + 1 + p2.z*p2.z)));;
-			normals[index + 4] = (float)(-1.0f / Math.sqrt((float)(p2.x*p2.x + 1 + p2.z*p2.z)));
-			normals[index + 5] = (float)(p2.z / Math.sqrt((float)(p2.x*p2.x + 1 + p2.z*p2.z)));
+			bottomVs.add(p2.z);
+			normals[index + 3] = p2.x /(float)Math.sqrt((p2.x*p2.x + 1 + p2.z*p2.z));;
+			bottomNs.add(normals[index + 3]);
+			normals[index + 4] = -1.0f /(float)Math.sqrt((p2.x*p2.x + 1 + p2.z*p2.z));
+			bottomNs.add(normals[index + 4]);
+			normals[index + 5] = p2.z /(float)Math.sqrt((p2.x*p2.x + 1 + p2.z*p2.z));
+			bottomNs.add(normals[index + 5]);
 		}
 		
 		//setting the endpoint
@@ -60,10 +82,28 @@ public class Cylinder extends TriangleMesh
 		
 		ArrayList<Integer> triangleAL = new ArrayList<Integer>();
 		//top circle:
-		for(int i = 3; i < vertices.length; i += 6){
+		for(int i = 3; i < topVs.size(); i += 3){
+			triangleAL.add(0);
 			triangleAL.add(i);
-			triangleAL.add(i + 1);
-			triangleAL.add(i + 2);
+			if(i + 3 >= topVs.size()){
+				triangleAL.add(3);
+				break;
+			} else
+				triangleAL.add(i + 3);
+		}
+		//bottom circle:
+		for(int i = 3; i < bottomVs.size(); i += 3){
+			triangleAL.add(0);
+			triangleAL.add(i);
+			if(i + 3 >= bottomVs.size()){
+				triangleAL.add(3);
+				break;
+			} else
+				triangleAL.add(i + 3);
+		}
+		//curved surface:
+		for(int i = 3; i < topVs.size(); i++){
+			
 		}
 		int[] triangles = new int[triangleAL.size()];
 		for(int i = 0; i < triangles.length; i++)
